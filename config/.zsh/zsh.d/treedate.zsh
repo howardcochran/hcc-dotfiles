bf68e23fab3464f03a7b7546b12f80f16164fe9b
Howard Cochran <cochran@lexmark.com>
2014-08-28 14:21:13 -0400

    zsh functions: Add treesize to sort file list by size
    Also add -r (reverse) option for treedate and treesize.
    Rename the script since it has more function now.

--- config/.zsh/zsh.d/treedate.zsh
+++
@@ -1,9 +0,0 @@
-
-# List all the files in the given directory tree(s), sorted by
-# modification date. Oldest to newest, because having the most
-# recent files at the end of the output us usually more convenient,
-# especially when not piped through a pager.
-function treedate() {
-	find "$@" -type f -printf "%T@ %u %g %Cc %k %p\n"| sort -n
-}
-
---
+++ config/.zsh/zsh.d/treesort.zsh
@@ -0,0 +1,32 @@
+# Internal function to short a full tree of files with the find format
+# string (and thus the sort criteria) parameterized.
+function __treesort() {
+	set -x
+	local fmt="$1"
+	shift
+	local reverse_arg=
+	if [ x"$1" = x'-r' ] ; then
+		reverse_arg="$1"
+		shift
+	fi
+	find "$@" -type f -printf "${fmt}"| sort -n $reverse_arg
+}
+
+# List all the files in the given directory tree(s), sorted by
+# modification date. Default oldest to newest, because having the most
+# recent files at the end of the output us usually more convenient,
+# especially when not piped through a pager.
+# Use -r to reverse the order (must be first arg)
+function treedate() {
+	__treesort "%T@ %u %g %Cc %k %p\n" "$@"
+}
+
+# List all the files in the given directory tree(s), sorted by
+# size. Default smallest to largest, because having the largest
+# files at the end of the output us usually more convenient,
+# especially when not piped through a pager.
+# Use -r to reverse the order (must be first arg)
+function treesize() {
+	__treesort "%s %u %g %Cc %k %p\n" "$@"
+}
+
