621a100c3d3a00c20b625afb25d2c6201e0ecbb5
Howard Cochran <cochran@lexmark.com>
2014-11-25 18:37:50 -0500

    Add findnotmp shell function to find but ignore tmp/

---
+++ config/.zsh/zsh.d/find-helpers.zsh
@@ -0,0 +1,15 @@
+
+# Do a find, pruning any directory tree called tmp. This is useful
+# for finding stuff in a bitbake / Yocto workspaces, while skipping build
+# output, which would be slow to search. e.g. build output is typically found
+# in poky/build/tmp within a poky workspace.
+# For convenience, we include -iname, so the first argument should be a
+# filename pattern trying to find, but since we use "$@", you can append other
+# find conditionals, e.g.:
+# findnotmp "recipes" -type d
+# The -print on the end of this command, somewhat counter-intuitively,
+# suppresses the print of the pruned directory names.
+function findnotmp () {
+    find . -path '*/tmp' -prune -o -iname "$@" -print
+}
+
