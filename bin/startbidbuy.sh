07b108315528bbaf154dd770292a619bbccf12d6
Howard Cochran <howard@badger-technologies.com>
2017-03-21 20:04:52 -0400

    Delete old cruft
    Old scriptlets and notes that are obsolete

--- bin/startbidbuy.sh
+++
@@ -1,6 +0,0 @@
-VBoxManage startvm BidBuy2009-1 --type gui
-VBoxManage startvm BidBuy2009-2 --type vrdp
-VBoxManage startvm BidBuy2009-3 --type vrdp
-VBoxManage startvm BidBuy2009-4 --type vrdp
-VBoxManage startvm BidBuy2009-5 --type vrdp
-VBoxManage startvm BidBuy2009-8 --type vrdp
