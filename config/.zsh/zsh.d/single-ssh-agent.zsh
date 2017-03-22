9f435b9d3e9ad06922115ef0ddb148fe9f33ea76
Howard Cochran <howard@badger-technologies.com>
2017-03-22 15:58:23 -0400

    single-ssh-agent.zsh: Do nothing if running under ssh agent forwarding

--- config/.zsh/zsh.d/single-ssh-agent.zsh
+++ config/.zsh/zsh.d/single-ssh-agent.zsh
@@ -3,14 +3,18 @@
 # Connect every new interactive shell owned by the current user
 # on this machine with the same ssh-agent, starting it if needed.
 
+# If we are runnning under ssh with agent forwarding, abort (do nothing)
+is_ssh_agent_forwarded() {
+	local ssh_pid=${SSH_AUTH_SOCK/#*\/ssh-*agent./}
+	[[ "$ssh_pid" =~ ^[0-9]\+$ ]] && [[ $(</proc/$ssh_pid/cmdline) = sshd* ]]
+}
+is_ssh_agent_forwarded && return 0
+
+
 # Stash away the original, in case user wants to restore it.
-# E.g. Useful if user is logging in via ssh with ssh-agent-forwarding.
-# TODO: Can we detect an ssh with agent-forwarding login and
-#       just do nothing in that case?
 if [ -n "$SSH_AUTH_SOCK" -a -z "$ORIG_SSH_AUTH_SOCK" ] ; then
 	ORIG_SSH_AUTH_SOCK="$SSH_AUTH_SOCK"
 fi
 
 unset SSH_AGENT_LAUNCHER  # Typically this was "upstart"
 eval $($HOME/bin/single-ssh-agent)
-
