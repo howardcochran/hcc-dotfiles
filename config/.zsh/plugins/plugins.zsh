dc68eab06666fe4b348efeea122eab4a459ac018
Howard Cochran <howard@badger-technologies.com>
2017-12-04 11:33:27 -0500

    zsh: Don't enable zsh-autosuggestions if zsh too old
    This plugin gave errors on Ubuntu 14.04 because it has zsh 5.0.1. It works
    fine on 16.04, which has zsh 5.1.1.

--- config/.zsh/plugins/plugins.zsh
+++ config/.zsh/plugins/plugins.zsh
@@ -1,6 +1,10 @@
 ZSH_PLUGDIR=${0:a:h}
 
-source $ZSH_PLUGDIR/zsh-autosuggestions/zsh-autosuggestions.zsh
+autoload -U is-at-least
+
+if is-at-least 5.1.1; then  # Not compatible with older
+    source $ZSH_PLUGDIR/zsh-autosuggestions/zsh-autosuggestions.zsh
+fi
 
 # ==== zsh-syntax-highlighting config ====
 # NOTE: This plugin must come LAST cuz it wraps all the other ZLE widgets
