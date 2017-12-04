23a1b97ba7fffe0013ecde497f3f2cc1f3910795
Howard Cochran <Howard_Cochran@jabil.com>
2017-12-04 12:12:14 -0500

    zsh-syntax-highlighting: Disable "cursor" highlighter
    To workaround "cursor disappears when moving back along the command" issue:
    https://github.com/zsh-users/zsh-syntax-highlighting/issues/171

--- config/.zsh/plugins/plugins.zsh
+++ config/.zsh/plugins/plugins.zsh
@@ -9,7 +9,7 @@ fi
 # ==== zsh-syntax-highlighting config ====
 # NOTE: This plugin must come LAST cuz it wraps all the other ZLE widgets
 source $ZSH_PLUGDIR/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets pattern cursor)
+ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets pattern)
 ZSH_HIGHLIGHT_STYLES[arg0]='fg=green,bold'
 ZSH_HIGHLIGHT_STYLES[globbing]='fg=blue,bold'
 ZSH_HIGHLIGHT_STYLES[comment]='fg=cyan,bold'
