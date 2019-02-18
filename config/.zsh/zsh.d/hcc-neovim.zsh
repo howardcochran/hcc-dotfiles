22085f60f4e7f6eea8fe9eecc268a5b0ef85383c
Howard Cochran <howard@badger-technologies.com>
2019-02-18 13:07:45 -0500

    Neovim: Set $VIRTUAL_ENV so python-based plugins work better
    See comments

--- config/.zsh/zsh.d/hcc-neovim.zsh
+++ config/.zsh/zsh.d/hcc-neovim.zsh
@@ -12,6 +12,17 @@ else
     nvim_socket="${nvim_sock_dir}/$$"
 fi
 
+
+# This will be copied to VIRTUAL_ENV in neovim's environment upon launch (see
+# below). But just store it in a local var for now because this shell may want
+# to work with a different python virtenv.
+# This is needed for python-based neovim plugins such as LanguageServer_python
+# to work right. Without this, it still works but launches a new language
+# server for every call, which is very slow and it eventually runs out of file
+# handles.
+nvim_virtual_env=~/ve/py2neovim
+[[ ! -d $nvim_virtual_env/bin ]] && nvim_virtual_env=''
+
 __find_nvim_job_and_socket() {
     jobs | sed -n '/ nvim --listen/ s/^\[\([0-9]\+\)\].* nvim --listen \([^ ]\+\).*/\1 \2/p;q'
 }
@@ -30,6 +41,11 @@ function v() {
     if [[ $#job_and_socket == 2 ]]; then
         local job=$job_and_socket[1]
         local socket=$job_and_socket[2]
+        # Quick hack because jobs output is sometimes too truncated to extract
+        # the socket number reliably. FIXME: Don't rely on jobs output for it
+        if [[ ! -e "$socket" ]]; then
+            socket=$nvim_socket
+        fi
         local args="--nostart $opt_tab $opt_split"
     else
         local job=
@@ -43,7 +59,7 @@ function v() {
         # it is what it takes to get the command's name in Zsh's job list to
         # be correct.
         local tmpfile=$(mktemp /tmp/vimstart.XXXXXX)
-        print nvim "--listen" "$socket" $opt_split "$@" '&' >> "$tmpfile"
+        print VIRTUAL_ENV=$nvim_virtual_env nvim "--listen" "$socket" $opt_split "$@" '&' >> "$tmpfile"
         source "$tmpfile"
         \rm "$tmpfile"
         fg
