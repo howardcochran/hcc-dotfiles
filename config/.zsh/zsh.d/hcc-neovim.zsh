f22a2eca69b445a2351813dcb7a6483996ccd6f2
Howard Cochran <howard@badger-technologies.com>
2019-08-29 18:04:10 -0400

    Neovim: Make v, vs, vts, more robust and simplify
    By relying on the output of "jobs" to find the neovim socket to connect to via
    nvr, it could break since the output of "jobs" was often truncated.  Now we
    just use a variable set when the shell starts.

--- config/.zsh/zsh.d/hcc-neovim.zsh
+++ config/.zsh/zsh.d/hcc-neovim.zsh
@@ -24,35 +24,17 @@ fi
 nvim_virtual_env=~/ve/py2neovim
 [[ ! -d $nvim_virtual_env/bin ]] && nvim_virtual_env=''
 
-__find_nvim_job_and_socket() {
-    jobs | sed -n '/ nvim --listen/ s/^\[\([0-9]\+\)\].* nvim --listen \([^ ]\+\).*/\1 \2/p;q'
+__find_nvim_job() {
+    jobs | sed -n '/ VIRTUAL_ENV=.*nvim --listen/ s/^\[\([0-9]\+\)\].* nvim --listen.*/\1/p;q'
 }
 
 function v() {
-    ## force_new=0
-    ## [ "$1" = '--new' ] && { force_new=1 ; shift }
     local opt_tab=''
     [ "$1" = '--newtab' ] && { opt_tab='-cc tabnew' ; shift }
     local opt_split=''
     [ "$1" = '--split' ] && { opt_split='-o' ; shift }
 
-    local -a job_and_socket
-    job_and_socket=( $(__find_nvim_job_and_socket) )
-
-    if [[ $#job_and_socket == 2 ]]; then
-        local job=$job_and_socket[1]
-        local socket=$job_and_socket[2]
-        # Quick hack because jobs output is sometimes too truncated to extract
-        # the socket number reliably. FIXME: Don't rely on jobs output for it
-        if [[ ! -e "$socket" ]]; then
-            socket=$nvim_socket
-        fi
-        local args="--nostart $opt_tab $opt_split"
-    else
-        local job=
-        local socket=$nvim_socket
-        local args="--listen $socket $opt_split"
-    fi
+    local job=$(__find_nvim_job)
 
     if [[ -z $job ]]; then
         mkdir -p "$nvim_sock_dir"
@@ -60,12 +42,12 @@ function v() {
         # it is what it takes to get the command's name in Zsh's job list to
         # be correct.
         local tmpfile=$(mktemp /tmp/vimstart.XXXXXX)
-        print VIRTUAL_ENV=$nvim_virtual_env nvim "--listen" "$socket" $opt_split "$@" '&' >> "$tmpfile"
+        print VIRTUAL_ENV=$nvim_virtual_env nvim "--listen" "$nvim_socket" $opt_split "$@" '&' >> "$tmpfile"
         source "$tmpfile"
         \rm "$tmpfile"
         fg
     else
-        NVIM_LISTEN_ADDRESS="$socket" nvr --nostart $opt_tab $opt_split "$@" &
+        NVIM_LISTEN_ADDRESS="$nvim_socket" nvr --nostart $opt_tab $opt_split "$@" &
         fg %$job
     fi
 }
