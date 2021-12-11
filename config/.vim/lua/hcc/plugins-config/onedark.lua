f98285a18360393f12157e6dea5e741d5839a8b9
Howard Cochran <howard@badger-technologies.com>
2021-12-11 00:50:33 -0500

    nvim: Tweak onedarkpro theme
    Swap many tree-sitter items blue<->red because typical code looked way
    too red and not nearly blue enough.
    
    Also make the red redder (looked a bit too orange for my taste).

--- config/.vim/lua/hcc/plugins-config/onedark.lua
+++ config/.vim/lua/hcc/plugins-config/onedark.lua
@@ -1,14 +1,17 @@
 local onedarkpro = require('onedarkpro')
 local utils = require('onedarkpro.utils')
+local config = config or require("onedarkpro.config").options
+
 -- See http://neovimcraft.com/plugin/olimorris/onedark.nvim/index.html
 local colors = {
   -- Unless otherwise noted, these are same as the theme. Copied here so can reference below.
-  bg = "#000028",     -- Darker and bluer than the theme
-  red = "#e06c75",
+  bg = "#000038",     -- Darker and bluer than the theme
+  -- red = "#e06c75", (Orig)
+  red = "#ff4060",
   yellow = "#e5c07b",
   green = "#98c379",
   cyan = "#56b6c2",
-  blue = "#61afef",
+  blue = "#61afff",
   black = "#1e1e1e",
   selection = "#002080", -- Much Bluer than the medium gray in the theme
   -- Extra colors not in theme. Reference below.
@@ -17,6 +20,11 @@ local colors = {
 }
 colors.color_column = utils.lighten(colors.bg, 0.97)
 colors.cursorline = utils.lighten(colors.bg, 0.95)
+local c = colors   -- For brevity
+
+local styles = {
+  functions = "NONE", -- This is default, but I need to reference this below.
+}
 
 onedarkpro.setup({
   colors = colors,
@@ -32,6 +40,37 @@ onedarkpro.setup({
     LspDiagnosticsSignInformation = { fg = "#FFFFFF", bg = utils.darken(colors.blue, 0.5) },
     LspDiagnosticsSignHint = { fg = "#FFFFFF", bg = utils.darken(colors.cyan, 0.5) },
 --    LspDiagnosticsVirtualTextError = { fg="#FFFFFF", bg="#FF0000" },
+    MatchParen = { fg = "#FFFFFF", style = "bold,underline" }, -- The character under the cursor or just before it, if it is a paired bracket, and its match. |pi_paren.txt|
+
+    -- This theme has too much red by default, so let's swap some of the red & blue items
+    -- Switch these from blue->red:
+    TSFunction = { fg = c.red, style = styles.functions }, -- For function (calls and definitions).
+    TSInclude = { fg = c.red, style = "italic" }, -- For includes: `#include` in C, `use` or `extern crate` in Rust, or `require` in Lua.
+    TSMethod = { fg = c.red }, -- For method calls and definitions.
+    TSURI = { fg = c.blue }, -- Any URI like a link or email.
+    cssFunctionName = { fg = c.red },
+    cssIdentifier = { fg = c.red },
+    jsDocParam = { fg = c.red },
+    jsFuncCall = { fg = c.red },
+    rubyFunction = { fg = c.red },
+    rubyInclude = { fg = c.red },
+
+    -- Switch these from red->blue:
+    TSField = { fg = c.blue },
+    TSParameter = { fg = c.blue, style = "italic" }, -- For parameters of a function.
+    TSProperty = { fg = c.blue }, -- Same as `TSField`.
+    TSSymbol = { fg = c.blue }, -- For identifiers referring to symbols or atoms.
+    TSTag = { fg = c.blue }, -- Tags like html tag names.
+    TSVariable = { fg = c.blue, style = config.styles.variables }, -- Any variable name that does not have another highlight.
+    cssBraces = { fg = c.blue },
+    cssTagName = { fg = c.blue },
+    jsSuper = { fg = c.blue },
+    jsTemplateBraces = { fg = c.blue },
+    jsThis = { fg = c.blue },
+    jsonKeyword = { fg = c.blue },
+    rubyGlobalVariable = { fg = c.blue },
+    rubyInterpolationDelimiter = { fg = c.blue },
+
   },
   options = {
     cursorline = true,
