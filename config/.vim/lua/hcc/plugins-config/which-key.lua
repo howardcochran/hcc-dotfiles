62ea9932b6984918a4051929f3b9c3d4c06c6539
Howard Cochran <howard@badger-technologies.com>
2021-12-11 00:39:19 -0500

    nvim: which-key: Fix major glitch in command mode
    My command-mode mappings of jk and kj for <Esc> and some Telescope calls
    starting with s cause which-key to try to open in command mode which
    renders completely wrong. So black-list which-key from these.

--- config/.vim/lua/hcc/plugins-config/which-key.lua
+++ config/.vim/lua/hcc/plugins-config/which-key.lua
@@ -1,1 +1,7 @@
-require('which-key').setup()
+require('which-key').setup({
+  triggers_blacklist = {
+    -- My mapping of jk and kj to <Esc> in command mode causes which-key to trigger
+    -- on j and k in command mode, which breaks horribly. So disable this.
+    c = { 'j', 'k' },
+  },
+})
