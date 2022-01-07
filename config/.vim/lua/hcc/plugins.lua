f32e7d622151ee78cf3f0ce32aebb532be128284
Howard Cochran <howard@badger-technologies.com>
2022-01-07 16:39:13 -0500

    nvim: Fix error bootstrapping Packer on first run
    Needed to packadd it before requiring it.

--- config/.vim/lua/hcc/plugins.lua
+++ config/.vim/lua/hcc/plugins.lua
@@ -1,7 +1,9 @@
 -- Bootstrap Packer
 local install_path = vim.fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+  print('Installing packer.')
   packer_bootstrap = vim.fn.system({'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path})
+  vim.cmd('packadd packer.nvim')
 end
 
 -- List of Plugins
