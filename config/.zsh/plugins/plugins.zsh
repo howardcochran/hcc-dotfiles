f78d9fbbb011bb9e71572c310d3622271a393f48
Howard Cochran <howard@badger-technologies.com>
2022-01-11 10:53:32 -0500

    zsh: Make all plugins optional
    For example, in-snap, plan to leave off the 'z' plugin

--- config/.zsh/plugins/plugins.zsh
+++ config/.zsh/plugins/plugins.zsh
@@ -3,10 +3,10 @@ ZSH_PLUGDIR=${0:a:h}
 autoload -U is-at-least
 
 # ==== Frecent directory changing plugin ====
-source $ZSH_PLUGDIR/z/z.sh
+[[ -f $ZSH_PLUGDIR/z/z.sh ]] && source $ZSH_PLUGDIR/z/z.sh
 
 # ==== zsh-autosuggestions config ====
-if is-at-least 5.1.1; then  # Not compatible with older
+if is-at-least 5.1.1 && [[ -f  $ZSH_PLUGDIR/zsh-autosuggestions/zsh-autosuggestions.zsh ]]; then
     source $ZSH_PLUGDIR/zsh-autosuggestions/zsh-autosuggestions.zsh
 
     # Make zsh-autosuggest ignore these wrapper widgets that I defined in
@@ -26,15 +26,14 @@ fi
 # NOTE: Prevent reloading of it if we re-source zsh dotfiles because this
 #       leads to buildup of zle wrapper widgets so that each re-source
 #       takes exponentially longer!
-if [[ -z "${FAST_BASE_DIR}" ]]; then
+if [[ -z "${FAST_BASE_DIR}" ]] && [[ -f ${ZSH_PLUGDIR}/fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh ]]; then
     FAST_WORK_DIR=XDG  # i.e. ~/.config/fsh. Must set before loding plugin
     source ${ZSH_PLUGDIR}/fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh
-fi
-
-if [[ ! -r ${FAST_WORK_DIR}/current_theme.zsh ]]; then
-    # First-time initialization of fast-syntax-highlighting
-    # Results are cached in $FAST_WORK_DIR
-    mkdir -p ${FAST_WORK_DIR}
-    fast-theme default
-    fast-theme ${ZSH_PLUGDIR}/highlighting-theme-overlay.ini
+    if [[ ! -r ${FAST_WORK_DIR}/current_theme.zsh ]]; then
+        # First-time initialization of fast-syntax-highlighting
+        # Results are cached in $FAST_WORK_DIR
+        mkdir -p ${FAST_WORK_DIR}
+        fast-theme default
+        fast-theme ${ZSH_PLUGDIR}/highlighting-theme-overlay.ini
+    fi
 fi
