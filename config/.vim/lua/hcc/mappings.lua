187c5ae11e45bd5ec06124bc7a0be514706f90d7
Charles Cochran <charles_cochran@jabil.com>
2023-05-31 11:41:34 -0400

    nvim: wildmenu: make <CR> advance to next subtree
    When using the up/down arrow keys to navigate the vertical wildmenu, the
    right arrow key advances to the next subtree. When using tab/shift-tab
    to navigate, however, there is no good way to do this. Pressing enter
    simply results in nvim opening the currently selected file/directory.
    Instead, when in a wildmenu, remap <CR> to <Down> so that pressing enter
    advances to the next subtree. Once the wildmenu is closed, pressing
    enter will open the chosen file/directory.

--- config/.vim/lua/hcc/mappings.lua
+++ config/.vim/lua/hcc/mappings.lua
@@ -63,12 +63,16 @@ map('n', 'du',  'dt_')
 
 -- Make the wildmenu behave sanely. The default mappings assume your wildmenu
 -- is layed out horizontally. But we have it configured vertically (by including 'pum'
--- in wildoptions), so the default bindings are very confusing. Fix that:
+-- in wildoptions), so the default bindings are very confusing.
+-- Also, while the wildmenu is visibile, remap enter to down so that enter can
+-- be used to advance to the next submenu (rather than open the selected
+-- file/directory).
 vim.cmd([[
   cnoremap <expr> <Up>    pumvisible() ? "\<Left>"  : "\<Up>"
   cnoremap <expr> <Down>  pumvisible() ? "\<Right>" : "\<Down>"
   cnoremap <expr> <Left>  pumvisible() ? "\<Up>"    : "\<Left>"
   cnoremap <expr> <Right> pumvisible() ? "\<Down>"  : "\<Right>"
+  cnoremap <expr> <CR> pumvisible() ? "\<Down>"  : "\<CR>"
 ]])
 -- NOTE: Due to probable bug as of Nvim v0.6.1, the equivalent Lua mappings,
 -- listed here, work functionally but cause rendering problems, so stick to Vim syntax for now:
