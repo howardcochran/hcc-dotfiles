5e28d0d86f362a9e1e6bb4565186e61a995796e2
Howard Cochran <howard_cochran@jabil.com>
2023-06-10 06:05:12 +0000

    nvim: cmp: Fix poor mappings - much better!
    Fix biggest annoyance with nvim-cmp completions: That pressing <CR> when
    the completion menu had popped up on its own (i.e. user had not slected
    an item with arrow keys or <Tab>) would complete the first item in the
    completion list rather than simply insert a newline, making typing prose
    very difficult whenever you neared the end of a line.
    
    Fix <Tab> and <S-Tab> to move forward & backward through the list rather
    than having <Tab> complete the first (or currently selected item). This
    is mostly for Charles's benefit, since he likes using <Tab> this way.
    
    Fix both <Down> and <Tab> to select the first (rather than second) item
    if no item has yet been selected. Now that <Tab> and <CR> no longer
    simply complete the first item, this has become important. Otherwise, it
    would be inconvenient to complete the first item in the list since <Tab>
    and <Down> would both skip that one. Now, you complete the first item
    via <Tab><CR> or <Down><CR> or <Tab><Space> or <Down><Space>.

--- config/.vim/lua/hcc/plugins/cmp.lua
+++ config/.vim/lua/hcc/plugins/cmp.lua
@@ -22,6 +22,7 @@ function M.config()
   local luasnip = require('luasnip')
 
   local has_words_before = function()
+    unpack = unpack or table.unpack
     local line, col = unpack(vim.api.nvim_win_get_cursor(0))
     return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
   end
@@ -36,46 +37,66 @@ function M.config()
       ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
       ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
       ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }), -- Start manual completion
-      ['<C-y>'] = cmp.config.disable, -- Removes the default `<C-y>` mapping (which is 'confirm')
       ['<C-e>'] = cmp.mapping({
         i = cmp.mapping.abort(),
         c = cmp.mapping.close(),
       }),
-      ['<CR>'] = cmp.mapping.confirm({ select = true }),
-      ['<Down>'] = cmp.mapping({i = cmp.mapping.select_next_item()}),
+      ['<Esc>'] = cmp.mapping({
+        i = function(fallback)
+          if cmp.visible() then
+            cmp.abort()
+          else
+            fallback()
+          end
+        end,
+        c = function(fallback)
+          if cmp.visible() then
+            cmp.close()
+          else
+            fallback()
+          end
+        end,
+      }),
       ['<Up>'] = cmp.mapping({i = cmp.mapping.select_prev_item()}),
-      -- - ['<Tab>'] = function(fallback)
-      -- -   cmp.select_next_item()
-      -- - end,
-      --['<Tab>'] = cmp.mapping(
-      --  function(fallback)
-      --    if cmp.visible() then
-      --      if cmp.core.view:get_selected_entry() then
-      --        cmp.core.view:_select(1, cmp.SelectBehavior.Insert)
-      --        -- cmp.select_next_item()
-      --      else
-      --        cmp.core.view:_select(1, cmp.SelectBehavior.Insert)
-      --      end
-      --    elseif luasnip.expand_or_jumpable() then
-      --      luasnip.expand_or_jump()
-      --    elseif has_words_before() then
-      --      cmp.complete()
-      --    else
-      --      fallback()
-      --    end
-      --  end,
-      --  { 'i', 's' }
-      --),
+      -- Special function needed to keep <Down> from skipping first entry when no selection yet.
+      ['<Down>'] = cmp.mapping(
+        function(callback)
+          if cmp.get_active_entry() then
+            cmp.select_next_item()
+          else
+            cmp.select_next_item({count = 0})
+          end
+        end,
+        { 'i', 'c', 's' }
+      ),
+      ['<CR>'] = cmp.mapping({
+        -- Only complete the item if explicitly selected (typ. via <Down> or <Tab>)
+        -- otherwise fallback to inserting a newline (canceling the menu in process.
+        -- This is really important to prevent last word on a line from being replaced
+        -- by the first completion item, making typing prose very hard!
+        i = function(fallback)
+          if cmp.visible() and cmp.get_active_entry() then
+            cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
+          else
+            fallback()
+          end
+        end,
+        s = cmp.mapping.confirm({ select = true }),
+        c = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
+      }),
       ['<Tab>'] = cmp.mapping(
         function(fallback)
           if cmp.visible() then
-            --I don't really like cycling through with <Tab>. Just accept current (or first) entry
-            --cmp.select_next_item()
-            cmp.confirm({select = false})
+            -- Keep Tab from skpping 1st entry when no selection yet.
+            if cmp.get_active_entry() then
+              cmp.select_next_item()
+            else
+              cmp.select_next_item({count = 0})
+            end
           elseif luasnip.expand_or_jumpable() then
             luasnip.expand_or_jump()
-          elseif has_words_before() then
-            cmp.complete()
+          --elseif has_words_before() then
+            --cmp.complete()
           else
             fallback()
           end
