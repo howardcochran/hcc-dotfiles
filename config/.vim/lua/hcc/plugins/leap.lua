106fb75e6b2e45c5c60953feffb680a305dddff9
Howard Cochran <howard_cochran@jabil.com>
2023-06-11 18:55:02 +0000

    nvim: Add leap.nvim (and vim-repeat)
    Finally going to join the "Motion plugin" club. Earlier things like
    EasyMotion seemed clunky and full of side-effects, while leap.nvim looks
    really useful and intuitive.
    
    It benefits from tpope/vim-repeat, so add that too. It may help some
    other plugins also.

---
+++ config/.vim/lua/hcc/plugins/leap.lua
@@ -0,0 +1,9 @@
+local M = {
+  'ggandor/leap.nvim',
+  lazy = false,
+  config = function()
+    require('leap').add_default_mappings()
+  end
+}
+
+return M
--- config/.vim/lua/hcc/plugins/misc.lua
+++ config/.vim/lua/hcc/plugins/misc.lua
@@ -1,6 +1,7 @@
 -- A few misc / small plugins with no configuration required:
 
 M = {
+  { 'tpope/vim-repeat', lazy = false },  -- Fix . with other plugins (like leap.nvim)
   { 'tpope/vim-eunuch', lazy = false },
   { 'lambdalisue/suda.vim', lazy = false },
   { 'nvim-tree/nvim-web-devicons', name = 'nvim-web-devicons' },
