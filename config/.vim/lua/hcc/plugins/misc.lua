72159df585b1de74afe8fcb9c0f008bf7969f7c8
Howard Cochran <howard_cochran@jabil.com>
2023-06-20 03:17:33 +0000

    nvim: Add nvim-unimpaired (old classic)
    Mostly want it for these simple mappings:
    [p , ]p  - Force linewise paste before or after current line.
    [b , ]b  - Cycle through buffer list
    [<space> - Add blank line before (after) current one, staying in normal
    yod      - Toggle diffthis / diffoff
    yor      - Toggle relativenumber
    yow      - Toggle wrap mode

--- config/.vim/lua/hcc/plugins/misc.lua
+++ config/.vim/lua/hcc/plugins/misc.lua
@@ -3,6 +3,7 @@
 M = {
   { 'tpope/vim-repeat', lazy = false },  -- Fix . with other plugins (like leap.nvim)
   { 'tpope/vim-eunuch', lazy = false },
+  { 'tpope/vim-unimpaired', lazy = false }, -- An old classic
   { 'lambdalisue/suda.vim', lazy = false },
   { 'nvim-tree/nvim-web-devicons', name = 'nvim-web-devicons' },
   -- For some reason, lazy doesn't setup Comment on its own, so provide my own config function here:
