2b9c4c33e7104d1392daeab737fd1ea85e08ef72
Howard Cochran <Howard_Cochran@jabil.com>
2023-09-03 22:27:51 -0400

    nvim: nvim-tree: Fix upstream incompatibility
    I had some dead code that was trying to `require("nvim-tree.config"),
    which is no longer possible. Since it was dead code anyway, just delete
    it.
    
    Also delete some old commented-out stuff

--- config/.vim/lua/hcc/plugins/nvim-tree.lua
+++ config/.vim/lua/hcc/plugins/nvim-tree.lua
@@ -4,26 +4,7 @@ local M = {
 }
 
 function M.config()
-  local tree_cb = require("nvim-tree.config").nvim_tree_callback
-
---  local function on_attach(bufnr)
---    local api = require('nvim-tree.api')
---
---    local function opts(desc)
---      return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
---    end
---
---    -- Apply default mappings
---    api.config.mappings.default_on_attach(bufnr)
---
---    vim.keymap.set('n', '<C-h>', api.tree.toggle_hidden_filter, opts('Toggle Dotfiles'))
---    vim.keymap.set('n', 'u', api.tree.change_root_to_parent, opts('Up'))
---    -- TODO: Map <Right> to expand when on directory (otherwise nothing)
---    -- ... and <Left> to collapse directory (Same as <BS>: tree_cb("close_node")
---  end
-
   require("nvim-tree").setup({
---      on_attach = on_attach,
     hijack_cursor = true,
     update_cwd = true,
     actions = {
