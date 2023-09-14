356d92b1810b67a00b7140c644aaa450fc5d8931
Howard Cochran <Howard_Cochran@jabil.com>
2023-09-13 21:41:44 -0400

    nvim: gitsigns: Fix upstream incompatibility
    Upstream completely changed the way key mappings must be specified.

--- config/.vim/lua/hcc/plugins/gitsigns.lua
+++ config/.vim/lua/hcc/plugins/gitsigns.lua
@@ -2,34 +2,54 @@ local M = {
   'lewis6991/gitsigns.nvim',
   dependencies = 'nvim-lua/plenary.nvim',
   event = 'BufReadPre',
+}
 
-  opts = {
-    keymaps = {
-      -- For consistency, use [h, ]h instead of default [c, ]c. Also simpler because we don't
-      -- need the &diff conditional.
-      ['n ]h'] = '<cmd>Gitsigns next_hunk<CR>',
-      ['n [h'] = '<cmd>Gitsigns prev_hunk<CR>',
+function M.config()
+  require('gitsigns').setup({
+    on_attach = function(bufnr)
+      local gs = package.loaded.gitsigns
+      local function map(mode, lhs, rhs, opts)
+        opts = opts or {}
+        opts.buffer = bufnr
+        vim.keymap.set(mode, lhs, rhs, opts)
+      end
 
-      ['n <leader>hB'] = '<cmd>Gitsigns toggle_current_line_blame<CR>',
+      local function my_next_hunk()
+        if vim.wo.diff then return ']c' end
+        vim.schedule(gs.next_hunk)
+        return '<Ignore>'
+      end
+      local function my_prev_hunk()
+        if vim.wo.diff then return '[c' end
+        vim.schedule(gs.prev_hunk)
+        return '<Ignore>'
+      end
 
-      -- These are defaults, copied here because this table repaces, rather than adds to, defaults:
-      ['n <leader>hs'] = '<cmd>Gitsigns stage_hunk<CR>',
-      ['v <leader>hs'] = ':Gitsigns stage_hunk<CR>',
-      ['n <leader>hu'] = '<cmd>Gitsigns undo_stage_hunk<CR>',
-      ['n <leader>hr'] = '<cmd>Gitsigns reset_hunk<CR>',
-      ['v <leader>hr'] = ':Gitsigns reset_hunk<CR>',
-      ['n <leader>hR'] = '<cmd>Gitsigns reset_buffer<CR>',
-      ['n <leader>hp'] = '<cmd>Gitsigns preview_hunk<CR>',
-      ['n <leader>hb'] = '<cmd>lua require"gitsigns".blame_line{full=true}<CR>',
-      ['n <leader>hS'] = '<cmd>Gitsigns stage_buffer<CR>',
-      ['n <leader>hU'] = '<cmd>Gitsigns reset_buffer_index<CR>',
+      -- Map to both ]h and ]c because used ]h in the past, but most people expect ]c
+      map('n', ']h', my_next_hunk, {expr=true})
+      map('n', '[h', my_prev_hunk, {expr=true})
+      map('n', ']c', my_next_hunk, {expr=true})
+      map('n', '[c', my_prev_hunk, {expr=true})
 
-      ['n <leader>tb'] = '<cmd>Gitsigns toggle_current_line_blame<CR>',
+      -- Suggestions from gitsigns author:
+      map('n', '<leader>hs', gs.stage_hunk)
+      map('n', '<leader>hr', gs.reset_hunk)
+      map('v', '<leader>hs', function() gs.stage_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
+      map('v', '<leader>hr', function() gs.reset_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
+      map('n', '<leader>hS', gs.stage_buffer)
+      map('n', '<leader>hu', gs.undo_stage_hunk)
+      map('n', '<leader>hR', gs.reset_buffer)
+      map('n', '<leader>hp', gs.preview_hunk)
+      map('n', '<leader>hb', function() gs.blame_line{full=true} end)
+      map('n', '<leader>tb', gs.toggle_current_line_blame)
+      map('n', '<leader>hd', gs.diffthis)
+      map('n', '<leader>hD', function() gs.diffthis('~') end)
+      map('n', '<leader>td', gs.toggle_deleted)
 
-      -- Text objects
-      ['o ih'] = ':<C-U>Gitsigns select_hunk<CR>',
-      ['x ih'] = ':<C-U>Gitsigns select_hunk<CR>'
-    },
+      -- map({'o', 'x'}, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+      map({'o', 'x'}, 'ih', gs.select_hunk)
+    end,
+
     current_line_blame_opts = {
       delay = 250,
       ignore_whitespace = true,
@@ -37,7 +57,7 @@ local M = {
     preview_config = {
       row = 1, -- Default is 0. Offset down one line so doesn't block view of cursor line
     },
-  },
-}
+  })
+end
 
 return M
